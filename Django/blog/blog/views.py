a2aa2dcec38df47130a6e5646c4f33c14de8c3ac
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2019-03-13 16:53:58 +0530

    Add files via upload

---
+++ Django/blog/blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render
+def blog(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def kope(request):
+    return JsonResponse({"hello " : "<h1>How Are You/h1>"})
+
+def portfolio(request):
+    return render(request,"test.html",{"resume":[{"name":"Shylesh","lname":"Unnithan","age":"19","skills":["Python","Python Django","C++"],"exp":["Intern at Spider Innovative","ECC, IEEE Kochi Hub","Vice-Chairman, CS Chapter IEEE SB CEC"]},
+    {"name":"Saranya","lname":"V Unnithan","age":"24","skills":["Speaking","Speech","C++"],"exp":["Intern at Incubation","Former Sports Captain KV Adoor"]}, {"name":"Rithu Maria","lname":"Jose","age":"19","skills":["Public Speaking","Speech","C++"],"exp":["Intern at Innostack","Kochi Hub Coordinator, CS Chapter IEEE Kerala Section","Vie Chairperson, WIE AG IEEE SB CEC"]}]})
+
+def test(request):
+    print(request.GET)
